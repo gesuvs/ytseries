2254329b9ebb5161a2827ba00365e02b6fb7537e
GBernardo10 <g_bernardo10@hotmail.com>
2020-01-27 19:46:48 -0200

    test

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
+const bodyParser = require('body-parser')
 const graphqlHttp = require('express-graphql')
